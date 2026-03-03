5b1e16c1b5aee046661acf0d92f43247dff2fcf3
Owner-Banci <murtazaevavazbek20@gmail.com>
2026-03-03 13:31:02 +0300

    проверка

--- DelegationApp/Core/Services/Mock/MockTaskService.swift
+++ DelegationApp/Core/Services/Mock/MockTaskService.swift
@@ -10,7 +10,7 @@ import Foundation
 final class MockTaskService: TaskService {
     func loadNearbyTasks() -> [TaskItem] {
         [
-            .init(title: "Купить молоко", price: 200, etaMinutes: 14, distanceKm: 1.1),
+            .init(title: "Купить молоко1,2,3", price: 200, etaMinutes: 14, distanceKm: 1.1),
             .init(title: "Забрать посылку", price: 400, etaMinutes: 10, distanceKm: 2.0),
             .init(title: "Доставить цветы", price: 500, etaMinutes: 18, distanceKm: 3.5),
             .init(title: "Помочь донести", price: 250, etaMinutes: 7, distanceKm: 0.6)
